62ffd24aecfb7567c59df0c2af3785f1f911f4d8
Jovy Ira <ira_bonching@yahoo.com>
2022-07-30 00:36:27 -0400

    added functions

---
+++ index.js
@@ -0,0 +1,151 @@
+// TODO: Include packages needed for this application
+const fs = require('fs/promises');
+const inquirer = require('inquirer');
+const {
+    generateMarkdown,
+    renderLicenseBadge,
+    renderLicenseLink,
+    renderLicenseSection
+  } = require('./utils/generateMarkdown.js')
+
+const readMeFormat = ({userName, email, projectTitle, illustration, license, inquirer, installation, repositoryDocument, documen, tests}) => {
+    return `
+# ${projectTitle}
+${renderLicenseBadge(license)}
+
+    
+
+## Description
+${illustration}
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents 
+
+(Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+
+## Installation
+${installation}
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+${repositoryDocument}
+
+Provide instructions and examples for use.
+
+${renderLicenseSection(license)}
+
+${renderLicenseLink(license)}
+
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. 
+If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. 
+ You might also want to consider adding the following sections.
+
+
+## Tests
+${tests}
+
+## Contact Info:
+
+*GitHub: https://github.com/${userName ?? ""}
+
+*Email:${email}
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+}
+
+//TODO: Create an array of questions for user input
+const questions= [
+    {
+        type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'userName',
+    },
+    {
+        type: 'input',
+        message: 'What is your Email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Write a short description of your project?',
+        name: 'illustration',
+    },
+    {
+        type: 'list',
+        message: 'What kind of licence should your project have?',
+        name: 'license',
+        choices: [
+            'MIT License', 
+            'Apache license 2.0', 
+            'GNU v3.0',
+            'N/A',
+        ]       
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependecies?',
+        name: 'inquirer',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using repo?',
+        name: 'repositoryDocument',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know contributing to the repo?',
+        name: 'document',
+    },
+    {
+        type: 'input',
+        message: 'What command  should be run to run test?',
+        name: 'tests',
+    },
+];
+
+
+
+
+//TODO: Create a function to initialize app
+function init() {
+inquirer.prompt(questions).then(({userName, email, projectTitle, illustration, license, inquirer, installation, repositoryDocument, document, tests}) => {
+ //TODO: Create a function to write README file 
+    const myReadme = readMeFormat ({userName, email, projectTitle, illustration, license, inquirer, installation, repositoryDocument, document, tests});
+    
+    fs.writeFile("README.md",myReadme);
+ 
+})
+
+}
+
+//Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,63 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+
+  let licenseKind = ''
+  if(license === 'MIT License') {
+    licenseKind = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (license === 'Apache license 2.0') {
+    licenseKind = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  } else if (license === 'GNU v3.0') {
+    licenseKind = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else {
+    licenseKind = ""
+  }
+  return licenseKind;
+};
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## license 
+  ${renderLicenseBadge(license)}`
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    let myLicenseLink = ""
+    if(license === 'MIT License') {
+      myLicenseLink = `(https://opensource.org/licenses/MIT)`
+    } else if (license === 'Apache license 2.0') {
+      myLicenseLink = `(https://opensource.org/licenses/Apache-2.0)`
+    } else if (license === 'GNU General Public License v3.0') {
+      myLicenseLink = `(https://www.gnu.org/licenses/gpl-3.0)`
+    } else {
+      myLicenseLink= ""
+    }
+    return myLicenseLink;
+  };
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let myLicenseSection = '## License';
+    if(license === 'N/A') {
+      return "";
+    }
+      return myLicenseSection;
+  }
+  
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+`;
+}
+
+module.exports = {
+  generateMarkdown,
+  renderLicenseBadge,
+  renderLicenseLink,
+  renderLicenseSection
+};
